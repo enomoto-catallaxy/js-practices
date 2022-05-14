80f03cf33739622b7ea96c3d09be78900128d299
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-14 17:41:51 +0900

    カレンダープログラムの修正5/14

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,53 +1,55 @@
-let today = new Date();
-var argv = require('minimist')(process.argv.slice(2));
-if(argv.m){
-  month = argv.m;
-}else{
-  month = today.getMonth() + 1;
-}
+const today = new Date()
+const argv = require('minimist')(process.argv.slice(2))
+let month = 0
+let year = 0
+if (argv.m) {
+  month = argv.m
+} else {
+  month = today.getMonth() + 1
+};
+if (argv.y) {
+  year = argv.y
+} else {
+  year = today.getFullYear()
+};
 
-if(argv.y){
-  year = argv.y;
-}else{
-  year = today.getFullYear();
-}
+const FirstDay = new Date(year, month - 1, 1)
+const LastDay = new Date(year, month, 0)
 
-let firstday = new Date(year, month - 1, 1); 
-let lastday = new Date(year, month, 0);
+const LastDate = LastDay.getDate()
+const days = []
+for (let i = 0; i < LastDate; i++) {
+  days.push(i + 1)
+  days[i] = String(days[i])
+};
 
-let last_date = lastday.getDate();
-let dayarrays = [];
-for(i = 0; i < last_date; i++){
-  dayarrays.push(i + 1);
-  dayarrays[i] = String(dayarrays[i]);
-}
-
-const dayname = ['日','月','火','水','木','金','土'];
-console.log('       ' + month + '月 ' + year);
-for(let i = 0; i < 7; i++){
-  process.stdout.write(dayname[i].padStart(2, ' '));
-}console.log();
+const DayNames = ['日', '月', '火', '水', '木', '金', '土']
+console.log('       ' + month + '月 ' + year)
+for (let i = 0; i < 7; i++) {
+  process.stdout.write(DayNames[i].padStart(2, ' '))
+};
+console.log()
 
 const eachSlice = (arr, n = 2, result = []) => {
   if (arr.length === 0) {
-    return result;
+    return result
   }
   result.push(arr.splice(0, n))
   return eachSlice(arr, n, result)
 }
-
-let first_dayofweek = firstday.getDay();
-let spacearrays = [];
-if(first_dayofweek != 0){
-  for(let i = 0; i < first_dayofweek; i++){
-    spacearrays[i] = ('  ');
+const FirstDayOfWeek = FirstDay.getDay()
+const spaces = []
+if (FirstDayOfWeek !== 0) {
+  for (let i = 0; i < FirstDayOfWeek; i++) {
+    spaces[i] = ('  ')
   }
 }
 
-let space_dayarrays = spacearrays.concat(dayarrays);
-let oneweekarrays = eachSlice([...space_dayarrays], 7);
-for(let i = 0; i < oneweekarrays.length; i++){
-  for(let j = 0; j < oneweekarrays[i].length; j++){
-    process.stdout.write(oneweekarrays[i][j].padStart(3, ' '));
-  }console.log();
+const IncludeSpaceInDays = spaces.concat(days)
+const SevenDays = eachSlice([...IncludeSpaceInDays], 7)
+for (let i = 0; i < SevenDays.length; i++) {
+  for (let j = 0; j < SevenDays[i].length; j++) {
+    process.stdout.write(SevenDays[i][j].padStart(3, ' '))
+  }
+  console.log()
 }
