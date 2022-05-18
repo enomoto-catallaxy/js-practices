ccf1130bb5673fadcc950c7c75d9eda26529e8c6
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-18 20:27:13 +0900

    カレンダープログラムの修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -6,28 +6,28 @@ if (argv.m) {
   month = argv.m
 } else {
   month = today.getMonth() + 1
-};
+}
 if (argv.y) {
   year = argv.y
 } else {
   year = today.getFullYear()
-};
+}
 
-const FirstDay = new Date(year, month - 1, 1)
-const LastDay = new Date(year, month, 0)
+const firstDay = new Date(year, month - 1, 1)
+const lastDay = new Date(year, month, 0)
 
-const LastDate = LastDay.getDate()
+const lastDate = lastDay.getDate()
 const days = []
-for (let i = 0; i < LastDate; i++) {
+for (let i = 0; i < lastDate; i++) {
   days.push(i + 1)
   days[i] = String(days[i])
-};
+}
 
-const DayNames = ['日', '月', '火', '水', '木', '金', '土']
+const dayNames = ['日', '月', '火', '水', '木', '金', '土']
 console.log('       ' + month + '月 ' + year)
 for (let i = 0; i < 7; i++) {
-  process.stdout.write(DayNames[i].padStart(2, ' '))
-};
+  process.stdout.write(dayNames[i].padStart(2, ' '))
+}
 console.log()
 
 const eachSlice = (arr, n, result = []) => {
@@ -37,19 +37,19 @@ const eachSlice = (arr, n, result = []) => {
   result.push(arr.splice(0, n))
   return eachSlice(arr, n, result)
 }
-const FirstDayOfWeek = FirstDay.getDay()
+const firstDayOfWeek = firstDay.getDay()
 const spaces = []
-if (FirstDayOfWeek !== 0) {
-  for (let i = 0; i < FirstDayOfWeek; i++) {
+if (firstDayOfWeek !== 0) {
+  for (let i = 0; i < firstDayOfWeek; i++) {
     spaces[i] = ('  ')
   }
 }
 
-const IncludeSpaceInDays = spaces.concat(days)
-const SevenDays = eachSlice([...IncludeSpaceInDays], 7)
-for (let i = 0; i < SevenDays.length; i++) {
-  for (let j = 0; j < SevenDays[i].length; j++) {
-    process.stdout.write(SevenDays[i][j].padStart(3, ' '))
+const includeSpaceInDays = spaces.concat(days)
+const sevenDays = eachSlice([...includeSpaceInDays], 7)
+for (let i = 0; i < sevenDays.length; i++) {
+  for (let j = 0; j < sevenDays[i].length; j++) {
+    process.stdout.write(sevenDays[i][j].padStart(3, ' '))
   }
   console.log()
 }
